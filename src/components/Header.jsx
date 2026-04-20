6293617f38c3b53f730dac9949dac7f39ce4865b
VitorRamires <vmr.rs@hotmail.com>
2026-04-20 11:07:38 -0300

    new: header customization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,17 @@ import logo from "../../public/logo.jpg";
 export function Header() {
   return (
     <header className="flex justify-between w-full items-center p-6 bg-zinc-900 sticky top-0 shadow-md shadow-grey-1000">
-      <img className="w-20 h-auto" src={logo} alt="food-order logo" />{" "}
-      <div className="cart items-end cursor-pointer text-amber-500">My Cart</div>
+      <div className="flex gap-3 flex-start justify-center items-center">
+        <img
+          className="w-20 h-auto rounded-full border-amber-500 border-2"
+          src={logo}
+          alt="food-order logo"
+        />
+        <span className="text-amber-500 font-bold">Food Order</span>
+      </div>
+      <div className="cart items-end cursor-pointer text-amber-500">
+        My Cart
+      </div>
     </header>
   );
 }
