30e17371ab71b4bbe64e0c9723af74e5e3880a15
VitorRamires <vmr.rs@hotmail.com>
2026-04-28 20:01:51 -0300

    new: sending order and customer info

--- src/DATA.js
+++ src/DATA.js
@@ -1,8 +1,29 @@
 export const URL_BASE = "http://localhost:3000";
-const API_URL = `${URL_BASE}/meals`
 
 export async function getMeals() {
-  const response = await fetch(API_URL);
+  const response = await fetch(`${URL_BASE}/meals`);
   const data = await response.json();
   return data;
+}
+
+export async function postMeals(orderData) {
+  console.log("🚀 Enviando:", JSON.stringify(orderData, null, 2));
+  try {
+    const response = await fetch(`${URL_BASE}/orders`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(orderData),
+    });
+
+    console.log("📦 Status:", response.status);
+    const data = await response.json();
+    console.log("📝 Response:", data);
+
+    if (response.status === 201) {
+      return { success: true };
+    }
+  } catch (error) {
+    console.error("❌ Erro:", error);
+    throw error;
+  }
 }
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,32 +1,93 @@
 import { useContext } from "react";
 import { Input } from "./Input";
 import { MealsOnCartContext } from "./context/MealsOnCart";
+import { useActionState } from "react";
+import { postMeals } from "../DATA";
 
 export function CheckoutForm() {
+  const { mealListOnCart } = useContext(MealsOnCartContext);
+  const [formState, actionHandleSumbit] = useActionState(handleSubmit, {
+    error: null,
+  });
 
-  const {mealListOnCart} = useContext(MealsOnCartContext)
+  async function handleSubmit(prevFormState, formData) {
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const street = formData.get("street");
+    const postalCode = formData.get("postalCode");
+    const city = formData.get("city");
 
-  function handleSubmit () {
-    console.log(mealListOnCart)
+    const customerInfos = {
+      name,
+      email,
+      street,
+      "postal-code": postalCode,
+      city,
+    };
+    const errors = [];
+
+    const orderData = {
+      order: {
+        items: mealListOnCart,
+        customer: customerInfos,
+      },
+    };
+
+    if (name === "") errors.push("fill the name input");
+    if (email === "") errors.push("fill the title input");
+    if (street === "") errors.push("fill the your opinion text area");
+    if (street === "") errors.push("fill the your opinion text area");
+    if (city === "") errors.push("fill the your opinion text area");
+
+    if (errors.length > 0) return { errors };
+
+    await postMeals(orderData);
+    return { errors: null };
   }
 
   return (
     <>
       <div className="checkout w-120">
-        <form className="p-5">
-          <Input width={"w-105"}  name={"Full name"} id={"name"} type={"text"} />
-          <Input width={"w-105"} name={"E-mail address"} id={"email"} type={"email"} />
-          <Input width={"w-105"} name={"Street"} id={"street"} type={"text"} />
+        <form action={actionHandleSumbit} className="p-5">
+          <Input
+            label={"Full name"}
+            id={"name"}
+            type={"text"}
+            width={"w-105"}
+          />
+          <Input
+            label={"E-mail address"}
+            id={"email"}
+            type={"email"}
+            width={"w-105"}
+          />
+          <Input label={"Street"} id={"street"} type={"text"} width={"w-105"} />
 
-          <div className="flex flex-row gap-5">
-            <Input width={"w-50"} name={"PostCard"} id={"postcard"} type={"number"} />
-            <Input width={"w-50"} name={"City"} id={"city"} type={"text"} />
+          <div classlabel="flex flex-row gap-5">
+            <Input
+              label={"Postal-Code"}
+              id={"postalCode"}
+              type={"text"}
+              width={"w-50"}
+            />
+            <Input label={"City"} id={"city"} type={"text"} width={"w-50"} />
           </div>
-        </form>
 
-        <button onClick={handleSubmit} className="w-full text-zinc-50 bg-amber-500  p-5 cursor-pointer hover:bg-amber-600 transition">
-          Submit Order
-        </button>
+          <button
+            className="w-full text-zinc-50 bg-amber-500  p-5 cursor-pointer hover:bg-amber-600 transition"
+            type="submit"
+          >
+            Submit Order
+          </button>
+
+          {formState.errors && (
+            <ul>
+              {formState.errors.map((error) => (
+                <li>{error}</li>
+              ))}
+            </ul>
+          )}
+        </form>
       </div>
     </>
   );
